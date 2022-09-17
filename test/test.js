524a8740fa7cb4ed4de6d93c73c05b438885ca49
lshoyfer <lshoyfer@gmail.com>
2022-09-17 17:15:21 -0400

    Working mdArray

--- test/test.js
+++ test/test.js
@@ -5,20 +5,22 @@
  * 
  */
 class MultiArray extends Array {
+    #dimensions;
     constructor(...args) {
         super();
-        console.log(args);
-        MultiArray.#createStack.call(this, 0);
+        this.#dimensions = [...args];
+        MultiArray.#createStack.call(this, 0, this);
     }
-    static #createStack(dim) {
-        console.log(this === MultiArray)
-        if (dim >= this.dimensions.length) return 0;
+    static #createStack(dim, arr) {
+        if (dim >= this.#dimensions.length) return 0;
         else {
-            for (let i = 0; i < dim; i++) this.push(MultiArray.#createStack.call(this, dim+1));
+            for (let i = 0; i < this.#dimensions[dim]; i++) arr.push(MultiArray.#createStack.call(this, dim+1, []));
+            return arr;
         }
     }
 
 }
 
 const test = new MultiArray(4, 3, 2);
+console.log(Reflect.ownKeys(test));
 console.log(test);
